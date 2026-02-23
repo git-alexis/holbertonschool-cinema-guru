774030699a2dcc624c3248631341219a232a27e7
git-alexis <alex59.billemont@gmail.com>
2026-02-23 18:01:39 +0100

    Ajout d'une requête api POST à "/api/auth/" pour modifier isLoggedIn et userUsername dans App.jsx

--- cinema-guru/src/App.jsx
+++ cinema-guru/src/App.jsx
@@ -1,5 +1,35 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { useEffect, useState } from "react";
+import "./App.css";
 
-export default App;
+//import Authentication from "./components/";
+//import Dashboard from "./components/";
+
+import api from "./services/api";
+
+export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userUsername, setUserUsername] = useState("");
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+
+    if (!token) return;
+
+    api.post("/api/auth/")
+      .then((response) => {
+        setIsLoggedIn(true);
+        setUserUsername(response.data.username);
+      })
+      .catch(() => {
+        setIsLoggedIn(false);
+        setUserUsername("");
+        localStorage.removeItem("accessToken");
+      });
+  }, []);
+
+  //if (isLoggedIn) {
+    //return <Dashboard />;
+  //}
+
+  //return <Authentication />;
+}
