64d184857d04211b68e1f3527895dd55575b3190
git-alexis <alex59.billemont@gmail.com>
2026-02-24 11:05:15 +0100

    Finalisation du processus d'authentification avec l'ajout de la fonction handleSubmit pour gérer les requêtes POST sur l'api login et register

--- cinema-guru/src/routes/auth/Authentication.jsx
+++ cinema-guru/src/routes/auth/Authentication.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./auth.css";
 import Login from "./Login";
 import Register from "./Register";
+import api from "../../services/api";
 
 export default function Authentication({
   setIsLoggedIn,
@@ -10,6 +11,39 @@ export default function Authentication({
   const [_switch, setSwitch] = useState(true);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  /* =========================
+     HANDLE SUBMIT
+  ========================== */
+  const handleSubmit = async (onSubmit) => {
+    onSubmit.preventDefault();
+
+    try {
+      let response;
+
+      if (_switch) {
+        response = await api.post("/api/auth/login", {
+          username,
+          password,
+        });
+      } else {
+        response = await api.post("/api/auth/register", {
+          username,
+          password,
+        });
+      }
+
+      const token = response.data.accessToken;
+      localStorage.setItem("accessToken", token);
+
+      setUserUsername(username);
+      setIsLoggedIn(true);
+
+    } catch (error) {
+      console.error("Authentication error:", error);
+      alert("Authentication failed");
+    }
+  };
 
   return (
     <div>
@@ -34,7 +68,7 @@ export default function Authentication({
       </div>
 
       {/* FORM */}
-      <form>
+      <form onSubmit={handleSubmit}>
 
         {_switch ? (
           <Login
