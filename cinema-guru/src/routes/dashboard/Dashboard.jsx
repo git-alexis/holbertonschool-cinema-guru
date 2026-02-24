9c957d0aedf5b157eb85377fbd1152dcb8fbccb7
git-alexis <alex59.billemont@gmail.com>
2026-02-24 15:51:02 +0100

    Déplacement du BrowserRouter et ajout du routage Dashboard

--- cinema-guru/src/routes/dashboard/Dashboard.jsx
+++ cinema-guru/src/routes/dashboard/Dashboard.jsx
@@ -1,22 +1,43 @@
 import "./dashboard.css";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Header from "../../components/navigation/Header";
 import SideBar from "../../components/navigation/SideBar";
+
+import HomePage from "../home/HomePage";
+import Favorites from "../favorites/Favorites";
+import WatchLater from "../watchlater/WatchLater";
 
 export default function Dashboard({
   userUsername,
   setIsLoggedIn,
 }) {
-
   return (
-    <div>
+    <BrowserRouter>
+      <div>
 
-      <Header
-        userUsername={userUsername}
-        setIsLoggedIn={setIsLoggedIn}
-      />
+        {/* Header */}
+        <Header
+          userUsername={userUsername}
+          setIsLoggedIn={setIsLoggedIn}
+        />
 
-      <SideBar />
+        {/* Sidebar */}
+        <SideBar />
 
-    </div>
+        {/* Routing */}
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/watchlater" element={<WatchLater />} />
+          <Route path="*" element={<Navigate to="/home" />} />
+        </Routes>
+
+      </div>
+    </BrowserRouter>
   );
 }
